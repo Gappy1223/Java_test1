cca1d2d722bec77ae5ad2acb6c748c8789bd7ca0
Gappy1223 <68161829+Gappy1223@users.noreply.github.com>
2024-10-26 16:27:51 -0600

    Se agregan clases Estudiante y Main

---
+++ Actividad9/Estudiante.java
@@ -0,0 +1,27 @@
+package Actividad9;
+
+public class Estudiante {
+    private String nombre;
+    private Integer edad;
+
+    public Estudiante(String nombre, Integer edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+}
